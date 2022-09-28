3ca078cdb169cb826330335c79237f70931a4987
ImamM5 <imamm@nycstudents.net>
2022-09-28 08:43:19 -0400

    New print statement and a variable

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,7 @@ public class HelloWorld
     public static void main (String [] args)
     {
         System.out.println("Hello World");
+        double x = 2.0 * 3;
+        System.out.println("Right now the time is "+ x+ " am.");
     }
 }
